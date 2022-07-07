8f548fdecac1711e2c2c95d374d78ad69de3ff39
lamvanhongUIT19520094 <19520094@gm.uit.edu.vn>
2022-07-07 21:38:49 +0700

    update project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="917e8fc7-6195-481f-b1ff-d84693184440"
+      userName="lamhong"
+      userSecret="lamvanhong"
+    />
   );
-}
+};
 
 export default App;
